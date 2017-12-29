76e5b614394c65676d316006659f4607cd6e96a9
Claus Matzinger <claus.matzinger+kb@gmail.com>
2017-12-29 19:23:32 +0100

    content-type header fix

--- ct-api/run.fsx
+++ ct-api/run.fsx
@@ -8,18 +8,11 @@ open Newtonsoft.Json
 
 type Ticker = {
     exchange: string
-    /// UNIX timestamp in ms (when the response was received)
     timestamp: uint64
-    /// The Pair corresponding to the Ticker returned (maybe useful later for asynchronous APIs)
     pair: string
-    /// Last trade price found in the history
     last_trade_price: string
-    /// Lowest ask price found in Orderbook
     lowest_ask: string
-    /// Highest bid price found in Orderbook
     highest_bid: string
-    // Bittrex does not support Volume for ticker so volume could be None
-    /// Last 24 hours volume (quote-volume)
     volume: uint64
 }
 
@@ -33,9 +26,9 @@ let Run(req: HttpRequestMessage, newTicker : byref<obj>, log: TraceWriter) =
         let tickerdata = JsonConvert.DeserializeObject<Ticker>(data)
         newTicker <- tickerdata
         let response = req.CreateResponse(HttpStatusCode.OK, JsonConvert.SerializeObject({message = "Ok"}))
-        response.Headers.Add("ContentType", "application/json")
+        response.Headers.Add("content-type", "application/json")
         response
     with e ->
         let response = req.CreateErrorResponse(HttpStatusCode.BadRequest, JsonConvert.SerializeObject({ message = e.ToString() } ))
-        response.Headers.Add("ContentType", "application/json")
+        response.Headers.Add("content-type", "application/json")
         response
